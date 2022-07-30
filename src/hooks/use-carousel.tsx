3c441f34c744e43d77fea70c3b4c90849fcd4da3
Leonel Meque <leomeque@gmail.com>
2022-07-30 15:21:11 +0200

    feat(hooks): add carousel hook

--- src/hooks/use-carousel.tsx
+++ src/hooks/use-carousel.tsx
@@ -1,36 +1,36 @@
 import { MouseEvent, useRef } from "react"
 
-
-export const useCarousel = ({ data }: { data: [] }) => {
+export const useCarousel = (data: any[]) => {
   const positionRef = useRef<number>(0)
 
-  const scrollToTarget = (position: number) => {
-    const selector = `.slide:nth-child(${position + 1})`
-    const slide = document.querySelector(selector)
-
-    slide.scrollIntoView({
-      behavior: "smooth",
-      inline: "end",
-      block: "nearest",
+  const scrollToTarget = (parent: Element, slidePosition: number) => {
+    const selector = `.slide-${slidePosition}`
+    parent.querySelector(selector).scrollIntoView({
+      behavior: "smooth",block:'nearest', inline:'start'
+      
     })
   }
 
-  const handleNextTarget = (event: MouseEvent) => {
-    if (positionRef.current > data.length) return
-    positionRef.current++
-    scrollToTarget(positionRef.current)
+  const handleNextTarget = (event?: MouseEvent) => {
+    const { currentTarget } = event
+    const { offsetParent } = currentTarget as HTMLButtonElement
+
+    if (positionRef.current >= data.length-1) positionRef.current = 0
+    else positionRef.current++
+    scrollToTarget(offsetParent, positionRef.current)
   }
 
-  const handlePreviousTarget = (event: MouseEvent) => {
-    if (positionRef.current < 0) return
+  const handlePreviousTarget = (event?: MouseEvent) => {
+    const { currentTarget } = event
+    const { offsetParent } = currentTarget as HTMLButtonElement
+
+    if (positionRef.current <= 0) return
     positionRef.current--
-    scrollToTarget(positionRef.current)
+    scrollToTarget(offsetParent, positionRef.current)
   }
 
   return {
     handleNextTarget,
-    handlePreviousTarget,
-    data
+    handlePreviousTarget
   }
-
 }
